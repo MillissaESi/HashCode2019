bbd7227fdae7091f025036169bb94a2674477889
Millissa Si Amer <millissa.si.amer@cern.ch>
2019-02-28 20:46:25 +0100

    first version

---
+++ Photo.py
@@ -0,0 +1,11 @@
+
+
+class Photo:
+
+    def __init__(self,id,m, tags, type):
+        self.id = id
+        self.size = m
+        self.tags = tags
+        self.type = type
+
+
---
+++ main.py
@@ -0,0 +1,17 @@
+
+import Photo
+
+def readInput(filename):
+    lines = [line.rstrip('\n') for line in open('filename')]
+    N= lines[0]
+    ID = 0
+    for i in range(1,N-1):
+        type =
+
+        photo = Photo(ID,)
+
+
+
+
+
+if __name__ == "__main__" :
